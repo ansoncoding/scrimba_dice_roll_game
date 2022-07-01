9a86790dd1c8e9e3b5b47359cc4aa7f091a55aaa
Sonia Zhang <53556427+ansoncoding@users.noreply.github.com>
2022-06-30 18:05:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const chanceBtn = document.getElementById("chanceBtn")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    chanceBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+/* Hook up a click event listener to the Roll Dice Button. */
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    }
+    player1Turn = !player1Turn
+})
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+chanceBtn.addEventListener("click", chanceRoll)
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "inline-block"
+    chanceBtn.style.display = "inline-block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
+
+function chanceRoll() {
+    const rand = Math.random()
+    console.log(rand)
+    let score = 0
+    if (rand > 0.5) {
+        const randomNumber = Math.floor(Math.random() * 6) + 1
+        score = randomNumber * 2
+    } else {
+        const randomNumber = Math.floor(Math.random() * 6) + 1
+        score = -randomNumber
+    }
+    if (player1Turn) {
+        player1Score += score
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = score
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += score
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = score
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    player1Turn = !player1Turn
+}
